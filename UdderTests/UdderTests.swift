36e18df22ed378654246f1b861605cf0ae691d87
Dezza Rizqi <rizqidezza@yahoo.com>
2024-10-22 16:37:23 +0700

    Initial Commit

---
+++ UdderTests/UdderTests.swift
@@ -0,0 +1,17 @@
+//
+//  UdderTests.swift
+//  UdderTests
+//
+//  Created by Dezza Rizqi on 22/10/24.
+//
+
+import Testing
+@testable import Udder
+
+struct UdderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
